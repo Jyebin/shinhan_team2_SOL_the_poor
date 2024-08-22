5edd3130b23aa9de842cbd1c6c29f72cbb8179da
soyoungxxx <otherplet@gmail.com>
2024-08-22 15:00:07 +0900

    feat: Create account entity file
    - 'account' 모델 작성

---
+++ src/main/java/com/choikang/poor/the_poor_back/model/Account.java
@@ -0,0 +1,38 @@
+package com.choikang.poor.the_poor_back.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.catalina.User;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountID;
+
+    @Column(length = 50, nullable = false)
+    private String accountName;
+
+    @Column(length = 50, nullable = false)
+    private String accountNumber;
+
+    @Column(length = 4, nullable = false)
+    private int accountPW;
+
+    @Column(nullable = false)
+    private int accountBalance;
+
+    @Column(nullable = false)
+    private boolean accountHasCan;
+
+    @Column
+    private int accountCanAmount;
+
+    @Column
+    private double accountCanInterestRate;
+}
