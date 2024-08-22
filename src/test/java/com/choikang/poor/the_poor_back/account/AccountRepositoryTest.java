ff8bf7f431fa77edd2c607266cf78406bca6f6ff
soyoungxxx <otherplet@gmail.com>
2024-08-22 15:22:02 +0900

    feat: Add Test
    - 테스트 기본 틀 추가

--- src/test/java/com/choikang/poor/the_poor_back/account/AccountRepositoryTest.java
+++ src/test/java/com/choikang/poor/the_poor_back/account/AccountRepositoryTest.java
@@ -1,4 +1,17 @@
 package com.choikang.poor.the_poor_back.account;
 
+import com.choikang.poor.the_poor_back.repository.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class AccountRepositoryTest {
+    @Autowired
+    private AccountRepository repository;
+
+    @Test
+    public void insertDummies() {
+
+    }
 }
--- src/test/java/com/choikang/poor/the_poor_back/user/UserRepositoryTest.java
+++ src/test/java/com/choikang/poor/the_poor_back/user/UserRepositoryTest.java
@@ -1,2 +1,11 @@
-package com.choikang.poor.the_poor_back.user;public class UserRepositoryTest {
+package com.choikang.poor.the_poor_back.user;
+
+import com.choikang.poor.the_poor_back.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository repository;
 }
