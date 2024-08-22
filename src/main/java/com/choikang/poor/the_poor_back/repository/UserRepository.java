d4ce4687bb3ab885289fa6f5624b357873aa56d0
soyoungxxx <otherplet@gmail.com>
2024-08-22 15:19:44 +0900

    feat: Add extend
    - extends JpaRepository 추가

--- src/main/java/com/choikang/poor/the_poor_back/repository/UserRepository.java
+++ src/main/java/com/choikang/poor/the_poor_back/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.choikang.poor.the_poor_back.repository;
 
-public interface UserRepository {
+import com.choikang.poor.the_poor_back.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
