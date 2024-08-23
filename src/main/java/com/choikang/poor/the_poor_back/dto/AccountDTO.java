f89f8c18d22fa3bae9b41f644be43dcc875655f6
soyoungxxx <otherplet@gmail.com>
2024-08-23 17:39:29 +0900

    feat: Add accountDTO

---
+++ src/main/java/com/choikang/poor/the_poor_back/dto/AccountDTO.java
@@ -0,0 +1,15 @@
+package com.choikang.poor.the_poor_back.dto;
+
+import lombok.*;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountDTO {
+    private String accountNumber;
+    private String accountName;
+    private int accountBalance;
+    private Boolean accountHasCan;
+}
