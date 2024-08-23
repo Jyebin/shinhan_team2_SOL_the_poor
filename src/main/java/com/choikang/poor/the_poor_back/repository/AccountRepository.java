e9f8e0fc5a973320799e2330fdebe6f1bbd304a9
soyoungxxx <otherplet@gmail.com>
2024-08-23 15:49:44 +0900

    feat: Add findHasCanByUserID
    - userID로 userHasCan 가져오는 쿼리문 작성

--- src/main/java/com/choikang/poor/the_poor_back/repository/AccountRepository.java
+++ src/main/java/com/choikang/poor/the_poor_back/repository/AccountRepository.java
@@ -2,6 +2,10 @@ package com.choikang.poor.the_poor_back.repository;
 
 import com.choikang.poor.the_poor_back.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    @Query("SELECT u.userHasCan FROM User u WHERE u.userID = :userID")
+    Boolean findHasCanByUserId(@Param("userId") Long userId);
 }
