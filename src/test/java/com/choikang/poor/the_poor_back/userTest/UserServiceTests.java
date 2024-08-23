3d483c88e7f743418e9e04697aa0fea0e215a61c
soyoungxxx <otherplet@gmail.com>
2024-08-23 17:33:04 +0900

    test: Create new test
    - 깡통 여부 판별 테스트

--- src/test/java/com/choikang/poor/the_poor_back/userTest/UserServiceTests.java
+++ src/test/java/com/choikang/poor/the_poor_back/userTest/UserServiceTests.java
@@ -1,2 +1,22 @@
-package com.choikang.poor.the_poor_back.userTest;public class UserServiceTests {
+package com.choikang.poor.the_poor_back.userTest;
+
+import com.choikang.poor.the_poor_back.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTests {
+    @Autowired
+    UserService userService;
+
+    @Test
+    @DisplayName("깡통 여부 판별 테스트")
+    public void findUserHasCanByUserID() {
+        Long userID = (long) 1;
+        boolean result = userService.findUserHasCanByUserID(userID);
+        Assertions.assertEquals(result, true);
+    }
 }
