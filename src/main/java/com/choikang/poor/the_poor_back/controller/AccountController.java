068a7274245a6feb975c0393031aeaa866521f91
soyoungxxx <otherplet@gmail.com>
2024-08-23 16:02:24 +0900

    feat: Add getAccountHasCan in controller
    - 프론트로 값 넘겨주는 코드 작성

--- src/main/java/com/choikang/poor/the_poor_back/controller/AccountController.java
+++ src/main/java/com/choikang/poor/the_poor_back/controller/AccountController.java
@@ -1,14 +1,16 @@
 package com.choikang.poor.the_poor_back.controller;
 
+import com.choikang.poor.the_poor_back.service.AccountService;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"http://localhost:3000/","http://localhost/"})
 @RestController
 @RequestMapping("/account")
 public class AccountController {
+    AccountService accountService;
+
     @GetMapping("/hasCan")
-    public Boolean getAccountHasCan(int userID) {
-        System.out.println(userID);
-        return false;
+    public Boolean getAccountHasCan(Long userID) {
+        return accountService.findUserHasCan(userID);
     }
 }
