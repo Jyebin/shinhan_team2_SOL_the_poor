2c20d42dc5c0359dd28490a5124beeda3500371b
soyoungxxx <otherplet@gmail.com>
2024-08-23 15:51:03 +0900

    feat: Add he has can function
    - 서비스 로직 작성

--- src/main/java/com/choikang/poor/the_poor_back/service/AccountService.java
+++ src/main/java/com/choikang/poor/the_poor_back/service/AccountService.java
@@ -1,11 +1,15 @@
 package com.choikang.poor.the_poor_back.service;
 
+import com.choikang.poor.the_poor_back.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
+    private final AccountRepository accountRepository;
 
-    public Boolean heHasCan(int userID) {
-
+    public Boolean heHasCan(Long userID) {
+        return accountRepository.findHasCanByUserId(userID);
     }
 }
