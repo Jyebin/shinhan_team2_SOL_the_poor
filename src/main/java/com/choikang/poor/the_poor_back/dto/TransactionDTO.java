465dec5d152c3a19e75877e5f4b3bc53be163258
soyoungxxx <otherplet@gmail.com>
2024-08-25 00:31:59 +0900

    feat: Add transaction to transactionDTO
    - DTO 변환 함수 생성

--- src/main/java/com/choikang/poor/the_poor_back/dto/TransactionDTO.java
+++ src/main/java/com/choikang/poor/the_poor_back/dto/TransactionDTO.java
@@ -1,6 +1,10 @@
 package com.choikang.poor.the_poor_back.dto;
 
+import com.choikang.poor.the_poor_back.model.Account;
+import com.choikang.poor.the_poor_back.model.Transaction;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Data
 @ToString
@@ -11,7 +15,23 @@ public class TransactionDTO {
     private String date;
     private String time;
     private String description;
-    private String status;
+    private Boolean status;
     private int amount;
     private int balance;
+
+    public static TransactionDTO convertToDTO(Transaction transaction) {
+        // date, time 분리
+        String dateAndTime = transaction.getTransactionDate().toString();
+        String date = dateAndTime.substring(0, 10);
+        String time = dateAndTime.substring(11, 19);
+
+        return TransactionDTO.builder()
+                .date(date)
+                .time(time)
+                .description(transaction.getTransactionName())
+                .status(transaction.getTransactionIsDeposit())
+                .amount(transaction.getTransactionMoney())
+                .balance(transaction.getTransactionBalance())
+                .build();
+    }
 }
