442a9ddb1c3c3f6631cbb2dc36e4701472b55281
soyoungxxx <otherplet@gmail.com>
2024-08-25 00:49:48 +0900

    test: Test get transaction list by account id
    - 테스트 코드 작성 및 테스트 완료

--- src/test/java/com/choikang/poor/the_poor_back/accountTest/AccountServiceTests.java
+++ src/test/java/com/choikang/poor/the_poor_back/accountTest/AccountServiceTests.java
@@ -1,7 +1,9 @@
 package com.choikang.poor.the_poor_back.accountTest;
 
 import com.choikang.poor.the_poor_back.dto.AccountDTO;
+import com.choikang.poor.the_poor_back.dto.TransactionDTO;
 import com.choikang.poor.the_poor_back.model.Account;
+import com.choikang.poor.the_poor_back.model.Transaction;
 import com.choikang.poor.the_poor_back.model.User;
 import com.choikang.poor.the_poor_back.repository.AccountRepository;
 import com.choikang.poor.the_poor_back.service.AccountService;
@@ -60,5 +62,36 @@ public class AccountServiceTests {
         Assertions.assertEquals("신한 슈퍼SOL 통장", accountDTO2.getName());
         Assertions.assertEquals(100000, accountDTO2.getBalance());
         Assertions.assertFalse(accountDTO2.getHasCan());
+    }
+
+    @Test
+    @DisplayName("accountID로 거래내역 찾기")
+    public void testGetTransactionsByAccountID() {
+        // Given: 테스트를 위한 데이터 준비
+        Account account = Account.builder().accountID(1L).build();
+
+        TransactionDTO transaction = TransactionDTO.builder()
+                .date("2024-08-12")
+                .time("11:35:48")
+                .balance(99912000)
+                .amount(100000)
+                .description("하소영")
+                .status(true)
+                .build();
+
+        // When: 계좌 정보 조회
+        List<TransactionDTO> transactions = accountService.getTransactionsByAccountID(account.getAccountID());
+
+        // Then: 계좌 정보 검증
+        Assertions.assertNotNull(transactions);
+        Assertions.assertEquals(5, transactions.size());
+
+        TransactionDTO transactionDTO = transactions.get(0);
+        Assertions.assertEquals(transaction.getDate(), transactionDTO.getDate());
+        Assertions.assertEquals(transaction.getTime(), transactionDTO.getTime());
+        Assertions.assertEquals(transaction.getBalance(), transactionDTO.getBalance());
+        Assertions.assertEquals(transaction.getAmount(), transactionDTO.getAmount());
+        Assertions.assertEquals(transaction.getDescription(), transactionDTO.getDescription());
+        Assertions.assertEquals(transaction.getStatus(), transactionDTO.getStatus());
     }
 }
