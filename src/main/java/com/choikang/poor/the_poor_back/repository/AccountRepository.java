3fefd6e34733182c6e21f8e0a7d5489fc8e6e797
soyoungxxx <otherplet@gmail.com>
2024-08-25 00:34:02 +0900

    feat: Add findBy
    - accountID로 거래내역 찾는 함수 작성

--- src/main/java/com/choikang/poor/the_poor_back/repository/AccountRepository.java
+++ src/main/java/com/choikang/poor/the_poor_back/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package com.choikang.poor.the_poor_back.repository;
 
 import com.choikang.poor.the_poor_back.dto.AccountDTO;
+import com.choikang.poor.the_poor_back.dto.TransactionDTO;
 import com.choikang.poor.the_poor_back.model.Account;
+import com.choikang.poor.the_poor_back.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,5 @@ import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findByUserUserID(Long userID);
+    List<Transaction> findByAccountAccountID(Long accountID);
 }
