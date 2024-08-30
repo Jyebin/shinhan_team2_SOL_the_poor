6644f3990bd1c1f596eace760741af092768f09a
Jyebin <yebin0322@naver.com>
2024-08-30 09:52:03 +0900

    fix : Change AttendancePostsController return value - modify to return [Type, Content] in content

--- src/main/java/com/choikang/poor/the_poor_back/controller/AttendancePostsController.java
+++ src/main/java/com/choikang/poor/the_poor_back/controller/AttendancePostsController.java
@@ -1,8 +1,10 @@
 package com.choikang.poor.the_poor_back.controller;
 
-import com.choikang.poor.the_poor_back.dto.AttendancePostsDTO;
-import com.choikang.poor.the_poor_back.model.AttendancePosts;
+import com.choikang.poor.the_poor_back.dto.AttendancePostsRequestDTO;
 import com.choikang.poor.the_poor_back.service.AttendancePostsService;
+import com.choikang.poor.the_poor_back.service.OAuth2UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +19,28 @@ public class AttendancePostsController {
     @Autowired
     private AttendancePostsService attendancePostsService;
 
+    @Autowired
+    private OAuth2UserService oAuth2UserService;
+
     @PostMapping("/create")
-    public ResponseEntity<String> createPost(@RequestBody AttendancePostsDTO attendancePostsDTO) {
-        try{
-            String responseContent = attendancePostsService.createPost(attendancePostsDTO);
+    public ResponseEntity<String[]> createPost(HttpServletRequest request,
+                                               @RequestBody AttendancePostsRequestDTO attendancePostsRequestDTO)
+            throws Exception {
+        String token = null;
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals("token")) {
+                token = cookie.getValue();
+            }
+        }
+        if (token == null) {
+            return new ResponseEntity<>(new String[0], HttpStatus.UNAUTHORIZED);
+        }
+        try {
+            Long userId = oAuth2UserService.getUserID(token);
+            attendancePostsRequestDTO.setUserId(userId);
+            String[] responseContent = attendancePostsService.createPost(attendancePostsRequestDTO);
             return new ResponseEntity<>(responseContent, HttpStatus.CREATED);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
