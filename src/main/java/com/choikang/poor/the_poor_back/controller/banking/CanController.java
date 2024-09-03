f07585eaecc00534ccae59a637dea756f356c9a1
soyoungxxx <otherplet@gmail.com>
2024-09-03 21:41:32 +0900

    refactor: Remove annotation

--- src/main/java/com/choikang/poor/the_poor_back/controller/banking/CanController.java
+++ src/main/java/com/choikang/poor/the_poor_back/controller/banking/CanController.java
@@ -7,7 +7,6 @@ import com.choikang.poor.the_poor_back.service.banking.CanService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
